5664b7cf3018b6379b27b19baaf5658b17eade78
Santiago <sanjoibarra007@gmail.com>
2022-02-09 16:59:19 -0300

    empezando patrones de render

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { TodoProvider } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoItem } from '../TodoItem';
+import { TodoList } from '../TodoList';
+import { TodoSearch } from '../TodoSearch';
 import { AppUI } from './AppUI';
-// import './App.css'; 
-
-
-
 
 function App() {
-
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader />
+      <TodoList />
+    </React.Fragment>
   );
 }
+function TodoHeader() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+    </React.Fragment>
+  );
+}
+function TodoList() {
+  return (    
+  <React.Fragment>
+    <TodoItem />
+  </React.Fragment>);
+}
+function App() {
+  return ();
+}
+
+
+
+// function App() {
+
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
